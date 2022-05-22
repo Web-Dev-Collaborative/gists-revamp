91013937613516b787f2e397a76d7788eeddcffe
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2022-05-22 20:41:32 +0000

    -update

--- dirhtml.py
+++ dirhtml.py
@@ -14,11 +14,11 @@ import sys
 from pathlib import Path
 from urllib.parse import quote
 
-DEFAULT_OUTPUT_FILE = "directory.html"
+DEFAULT_OUTPUT_FILE = 'index.html'
 
 
 def process_dir(top_dir, opts):
-    glob_patt = opts.filter or "*"
+    glob_patt = opts.filter or '*'
 
     path_top_dir: Path
     path_top_dir = Path(top_dir)
@@ -27,53 +27,60 @@ def process_dir(top_dir, opts):
     index_path = Path(path_top_dir, opts.output_file)
 
     if opts.verbose:
-        print(f"Traversing dir {path_top_dir.absolute()}")
+        print(f'Traversing dir {path_top_dir.absolute()}')
 
     try:
-        index_file = open(index_path, "w")
+        index_file = open(index_path, 'w')
     except Exception as e:
-        print("cannot create file %s %s" % (index_path, e))
+        print('cannot create file %s %s' % (index_path, e))
         return
 
-    index_file.write(
-        """<!DOCTYPE html>
+    index_file.write("""<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
     * { padding: 0; margin: 0; }
+
     body {
         font-family: sans-serif;
         text-rendering: optimizespeed;
         background-color: #ffffff;
     }
+
     a {
         color: #006ed3;
         text-decoration: none;
     }
+
     a:hover,
     h1 a:hover {
         color: #319cff;
     }
+
     header,
     #summary {
         padding-left: 5%;
         padding-right: 5%;
     }
+
     th:first-child,
     td:first-child {
         width: 5%;
     }
+
     th:last-child,
     td:last-child {
         width: 5%;
     }
+
     header {
         padding-top: 25px;
         padding-bottom: 15px;
         background-color: #f2f2f2;
     }
+
     h1 {
         font-size: 20px;
         font-weight: normal;
@@ -82,19 +89,24 @@ def process_dir(top_dir, opts):
         text-overflow: ellipsis;
         color: #999;
     }
+
     h1 a {
         color: #000;
         margin: 0 4px;
     }
+
     h1 a:hover {
         text-decoration: underline;
     }
+
     h1 a:first-child {
         margin: 0;
     }
+
     main {
         display: block;
     }
+
     .meta {
         font-size: 12px;
         font-family: Verdana, sans-serif;
@@ -102,63 +114,79 @@ def process_dir(top_dir, opts):
         padding-top: 10px;
         padding-bottom: 10px;
     }
+
     .meta-item {
         margin-right: 1em;
     }
+
     #filter {
         padding: 4px;
         border: 1px solid #CCC;
     }
+
     table {
         width: 100%;
         border-collapse: collapse;
     }
+
     tr {
         border-bottom: 1px dashed #dadada;
     }
+
     tbody tr:hover {
         background-color: #ffffec;
     }
+
     th,
     td {
         text-align: left;
         padding: 10px 0;
     }
+
     th {
         padding-top: 15px;
         padding-bottom: 15px;
         font-size: 16px;
         white-space: nowrap;
     }
+
     th a {
         color: black;
     }
+
     th svg {
         vertical-align: middle;
     }
+
     td {
         white-space: nowrap;
         font-size: 14px;
     }
+
     td:nth-child(2) {
         width: 80%;
     }
+
     td:nth-child(3) {
         padding: 0 20px 0 20px;
     }
+
     th:nth-child(4),
     td:nth-child(4) {
         text-align: right;
     }
+
     td:nth-child(2) svg {
         position: absolute;
     }
+
     td .name {
         margin-left: 1.75em;
         word-break: break-all;
         overflow-wrap: break-word;
         white-space: pre-wrap;
     }
+
     td .goup {
         margin-left: 1.75em;
         padding: 0;
@@ -166,36 +194,45 @@ def process_dir(top_dir, opts):
         overflow-wrap: break-word;
         white-space: pre-wrap;
     }
+
     .icon {
         margin-right: 5px;
     }
+
     tr.clickable { 
         cursor: pointer; 
     } 
     tr.clickable a { 
         display: block; 
     } 
+
     @media (max-width: 600px) {
+
         * {
             font-size: 1.06rem;
         }
         .hideable {
             display: none;
         }
+
         td:nth-child(2) {
             width: auto;
         }
+
         th:nth-child(3),
         td:nth-child(3) {
             padding-right: 5%;
             text-align: right;
         }
+
         h1 {
             color: #000;
         }
+
         h1 a {
             margin: 0;
         }
+
         #filter {
             max-width: 100px;
         }
@@ -209,6 +246,7 @@ def process_dir(top_dir, opts):
         <g id="go-up">
             <path d="M10,9V5L3,12L10,19V14.9C15,14.9 18.5,16.5 21,20C20,15 17,10 10,9Z" fill="#696969"/>
         </g>
+
         <!-- Folder -->
         <g id="folder" fill-rule="nonzero" fill="none">
             <path d="M285.22 37.55h-142.6L110.9 0H31.7C14.25 0 0 16.9 0 37.55v75.1h316.92V75.1c0-20.65-14.26-37.55-31.7-37.55z" fill="#FFA000"/>
@@ -223,6 +261,7 @@ def process_dir(top_dir, opts):
                 <path d="M126.154134,250.559184 C126.850974,251.883673 127.300549,253.006122 127.772602,254.106122 C128.469442,255.206122 128.919016,256.104082 129.638335,257.002041 C130.559962,258.326531 131.728855,259 133.100057,259 C134.493737,259 135.415364,258.55102 136.112204,257.67551 C136.809044,257.002041 137.258619,255.902041 137.258619,254.577551 C137.258619,253.904082 137.258619,252.804082 137.033832,251.457143 C136.786566,249.908163 136.561779,249.032653 136.561779,248.583673 C136.089726,242.814286 135.864939,237.920408 135.864939,233.273469 C135.864939,225.057143 136.786566,217.514286 138.180246,210.846939 C139.798713,204.202041 141.889234,198.634694 144.429328,193.763265 C147.216689,188.869388 150.678411,184.873469 154.836973,181.326531 C158.995535,177.779592 163.626149,174.883673 168.481552,172.661224 C173.336954,170.438776 179.113983,168.665306 185.587852,167.340816 C192.061722,166.218367 198.760378,165.342857 205.481514,164.669388 C212.18017,164.220408 219.598146,163.995918 228.162535,163.995918 L246.055591,163.995918 L246.055591,195.514286 C246.055591,197.736735 246.752431,199.510204 248.370899,201.059184 C250.214153,202.608163 252.079886,203.506122 254.372715,203.506122 C256.463236,203.506122 258.531277,202.608163 260.172223,201.059184 L326.102289,137.797959 C327.720757,136.24898 328.642384,134.47551 328.642384,132.253061 C328.642384,130.030612 327.720757,128.257143 326.102289,126.708163 L260.172223,63.4469388 C258.553756,61.8979592 256.463236,61 254.395194,61 C252.079886,61 250.236632,61.8979592 248.393377,63.4469388 C246.77491,64.9959184 246.07807,66.7693878 246.07807,68.9918367 L246.07807,100.510204 L228.162535,100.510204 C166.863084,100.510204 129.166282,117.167347 115.274437,150.459184 C110.666301,161.54898 108.350993,175.310204 108.350993,191.742857 C108.350993,205.279592 113.903236,223.912245 124.760454,247.438776 C125.00772,248.112245 125.457294,249.010204 126.154134,250.559184 Z" id="Shape" fill="#FFFFFF" transform="translate(218.496689, 160.000000) scale(-1, 1) translate(-218.496689, -160.000000) "></path>
             </g>
         </g>
+
         <!-- File -->
         <g id="file" stroke="#000" stroke-width="25" fill="#FFF" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
             <path d="M13 24.12v274.76c0 6.16 5.87 11.12 13.17 11.12H239c7.3 0 13.17-4.96 13.17-11.12V136.15S132.6 13 128.37 13H26.17C18.87 13 13 17.96 13 24.12z"/>
@@ -241,8 +280,8 @@ def process_dir(top_dir, opts):
     </svg>
 <header>
     <h1>"""
-        f"{path_top_dir.name}"
-        """</h1>
+                     f'{path_top_dir.name}'
+                     """</h1>
                  </header>
                  <main>
                  <div class="listing">
@@ -267,18 +306,15 @@ def process_dir(top_dir, opts):
                              <td class="hideable">&mdash;</td>
                              <td class="hideable"></td>
                          </tr>
-                 """
-    )
+                 """)
 
     # sort dirs first
-    sorted_entries = sorted(
-        path_top_dir.glob(glob_patt), key=lambda p: (p.is_file(), p.name)
-    )
+    sorted_entries = sorted(path_top_dir.glob(glob_patt), key=lambda p: (p.is_file(), p.name))
 
     entry: Path
     for entry in sorted_entries:
 
-        # don't include directory.html in the file listing
+        # don't include index.html in the file listing
         if entry.name.lower() == opts.output_file.lower():
             continue
 
@@ -287,55 +323,50 @@ def process_dir(top_dir, opts):
 
         # From Python 3.6, os.access() accepts path-like objects
         if (not entry.is_symlink()) and not os.access(str(entry), os.W_OK):
-            print(
-                f"*** WARNING *** entry {entry.absolute()} is not writable! SKIPPING!"
-            )
+            print(f"*** WARNING *** entry {entry.absolute()} is not writable! SKIPPING!")
             continue
         if opts.verbose:
-            print(f"{entry.absolute()}")
+            print(f'{entry.absolute()}')
 
         size_bytes = -1  ## is a folder
-        size_pretty = "&mdash;"
-        last_modified = "-"
-        last_modified_human_readable = "-"
-        last_modified_iso = ""
+        size_pretty = '&mdash;'
+        last_modified = '-'
+        last_modified_human_readable = '-'
+        last_modified_iso = ''
         try:
             if entry.is_file():
                 size_bytes = entry.stat().st_size
                 size_pretty = pretty_size(size_bytes)
 
             if entry.is_dir() or entry.is_file():
-                last_modified = datetime.datetime.fromtimestamp(
-                    entry.stat().st_mtime
-                ).replace(microsecond=0)
+                last_modified = datetime.datetime.fromtimestamp(entry.stat().st_mtime).replace(microsecond=0)
                 last_modified_iso = last_modified.isoformat()
                 last_modified_human_readable = last_modified.strftime("%c")
 
         except Exception as e:
-            print("ERROR accessing file name:", e, entry)
+            print('ERROR accessing file name:', e, entry)
             continue
 
         entry_path = str(entry.name)
 
         if entry.is_dir() and not entry.is_symlink():
-            entry_type = "folder"
-            if os.name not in ("nt",):
+            entry_type = 'folder'
+            if os.name not in ('nt',):
                 # append trailing slash to dirs, unless it's windows
-                entry_path = os.path.join(entry.name, "")
+                entry_path = os.path.join(entry.name, '')
 
         elif entry.is_dir() and entry.is_symlink():
-            entry_type = "folder-shortcut"
-            print("dir-symlink", entry.absolute())
+            entry_type = 'folder-shortcut'
+            print('dir-symlink', entry.absolute())
 
         elif entry.is_file() and entry.is_symlink():
-            entry_type = "file-shortcut"
-            print("file-symlink", entry.absolute())
+            entry_type = 'file-shortcut'
+            print('file-symlink', entry.absolute())
 
         else:
-            entry_type = "file"
+            entry_type = 'file'
 
-        index_file.write(
-            f"""
+        index_file.write(f"""
         <tr class="file">
             <td></td>
             <td>
@@ -348,35 +379,33 @@ def process_dir(top_dir, opts):
             <td class="hideable"><time datetime="{last_modified_iso}">{last_modified_human_readable}</time></td>
             <td class="hideable"></td>
         </tr>
-"""
-        )
+""")
 
-    index_file.write(
-        """
+    index_file.write("""
             </tbody>
         </table>
     </div>
 </main>
 </body>
-</html>"""
-    )
+</html>""")
     if index_file:
         index_file.close()
 
 
 # bytes pretty-printing
 UNITS_MAPPING = [
-    (1024 ** 5, " PB"),
-    (1024 ** 4, " TB"),
-    (1024 ** 3, " GB"),
-    (1024 ** 2, " MB"),
-    (1024 ** 1, " KB"),
-    (1024 ** 0, (" byte", " bytes")),
+    (1024 ** 5, ' PB'),
+    (1024 ** 4, ' TB'),
+    (1024 ** 3, ' GB'),
+    (1024 ** 2, ' MB'),
+    (1024 ** 1, ' KB'),
+    (1024 ** 0, (' byte', ' bytes')),
 ]
 
 
 def pretty_size(bytes, units=UNITS_MAPPING):
     """Human-readable file sizes.
+
     ripped from https://pypi.python.org/pypi/hurry.filesize/
     """
     for factor, suffix in units:
@@ -394,50 +423,37 @@ def pretty_size(bytes, units=UNITS_MAPPING):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="""DESCRIPTION:
+    parser = argparse.ArgumentParser(description='''DESCRIPTION:
     Generate directory index files (recursive is OFF by default).
     Start from current dir or from folder passed as first positional argument.
-    Optionally filter by file types with --filter "*.py". """
-    )
+    Optionally filter by file types with --filter "*.py". ''')
 
-    parser.add_argument(
-        "top_dir",
-        nargs="?",
-        action="store",
-        help="top folder from which to start generating indexes, "
-        "use current folder if not specified",
-        default=os.getcwd(),
-    )
+    parser.add_argument('top_dir',
+                        nargs='?',
+                        action='store',
+                        help='top folder from which to start generating indexes, '
+                             'use current folder if not specified',
+                        default=os.getcwd())
 
-    parser.add_argument(
-        "--filter", "-f", help="only include files matching glob", required=False
-    )
+    parser.add_argument('--filter', '-f',
+                        help='only include files matching glob',
+                        required=False)
 
-    parser.add_argument(
-        "--output-file",
-        "-o",
-        metavar="filename",
-        default=DEFAULT_OUTPUT_FILE,
-        help=f'Custom output file, by default "{DEFAULT_OUTPUT_FILE}"',
-    )
+    parser.add_argument('--output-file', '-o',
+                        metavar='filename',
+                        default=DEFAULT_OUTPUT_FILE,
+                        help=f'Custom output file, by default "{DEFAULT_OUTPUT_FILE}"')
 
-    parser.add_argument(
-        "--recursive",
-        "-r",
-        action="store_true",
-        help="recursively process nested dirs (FALSE by default)",
-        required=False,
-    )
+    parser.add_argument('--recursive', '-r',
+                        action='store_true',
+                        help="recursively process nested dirs (FALSE by default)",
+                        required=False)
 
-    parser.add_argument(
-        "--verbose",
-        "-v",
-        action="store_true",
-        help="***WARNING: can take longer time with complex file tree structures on slow terminals***"
-        " verbosely list every processed file",
-        required=False,
-    )
+    parser.add_argument('--verbose', '-v',
+                        action='store_true',
+                        help='***WARNING: can take longer time with complex file tree structures on slow terminals***'
+                             ' verbosely list every processed file',
+                        required=False)
 
     config = parser.parse_args(sys.argv[1:])
     process_dir(config.top_dir, config)
